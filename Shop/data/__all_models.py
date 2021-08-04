881b41ef05238c8a9bb778ebc001708be3849650
AllaSim <79840796+AllaSim@users.noreply.github.com>
2021-08-04 10:34:55 +0300

    Delete Shop directory

--- Shop/data/__all_models.py
+++
@@ -1,5 +0,0 @@
-from . import user
-from . import courier
-from . import goods
-from . import orders
-from . import filling
--- Shop/forms/courier_order.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-
-
-class CourierOrder(FlaskForm):
-    order_id = StringField('Номер заказа')
-    address = StringField('Адрес доставки')
